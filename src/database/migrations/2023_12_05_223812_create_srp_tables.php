44a643ebfa6744c421c8ab929188f2ab41e72607
Crypta Electrica <crypta@crypta.tech>
2023-12-10 19:11:17 +1030

    fix: correct price source to be uint

--- src/database/migrations/2023_12_05_223812_create_srp_tables.php
+++ src/database/migrations/2023_12_05_223812_create_srp_tables.php
@@ -41,7 +41,7 @@ class CreateSrpTables extends Migration
             $table->string('rule_type');
             $table->integer('type_id')->nullable()->unique();
             $table->integer('group_id')->nullable()->unique();
-            $table->string('price_source')->default('internal');
+            $table->unsignedInteger('price_source');
             $table->bigInteger('base_value')->default(0);
             $table->integer('hull_percent')->default(0);
             $table->integer('fit_percent')->default(0);
